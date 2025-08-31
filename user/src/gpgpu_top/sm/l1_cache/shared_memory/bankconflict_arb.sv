d4a683d5d5a236b322e5d2095cd21e844e0c0fde
paradox99926 <l.ming.yu@outlook.com>
2025-09-01 00:41:48 +0800

    add

--- user/src/gpgpu_top/sm/l1_cache/shared_memory/bankconflict_arb.sv
+++ user/src/gpgpu_top/sm/l1_cache/shared_memory/bankconflict_arb.sv
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2023-2024 C*Core Technology Co.,Ltd,Suzhou.
- * Ventus-RTL is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PSL v2 for more details. */
-// Author: Tan, Zhiyuan
-// Description: For dealing with bank conflict: split one require into multiple requre
 `timescale 1ns/1ns
 
 `include "define.v"
